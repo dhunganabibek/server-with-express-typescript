ed6b228c4e2684c3ccb06fe1908d2bede156261d
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-05-23 23:22:09 -0500

    Hello world api completed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <div>
+    <h1>Hello World</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Application listening at port 3000");
+});
